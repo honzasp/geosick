f5aae8875a2dce68bde8e3293a6d28b6cad2451e
Jan Špaček <patek.mail@gmail.com>
2020-03-17 12:39:50 +0100

    Document the API a bit

--- geosick.py
+++ geosick.py
@@ -2,7 +2,17 @@ import collections
 
 ## API
 
-# Sample of a user location.
+# Sample of a user location. The samples for a user must be ordered by timestamp
+# (increasing).
+#
+# We assume that the samples are taken with a steady rate, so we can reasonably
+# interpolate between samples. To indicate that there is a gap in the data (i.e., the GPS
+# signal was lost or the collection was stopped), mark the last sample before the gap with
+# `is_end = True`: we will assume that there is no knowledge of the user position between
+# the sample with `is_gap = True` and the following sample.
+#
+# The horizontal and vertical accuracy are assumed to be maximum "reasonable" distance of
+# the true position from the indicated position: it is NOT a standard deviation.
 UserSample = collections.namedtuple("UserSample", [
     "timestamp", # Timestamp in ms (unix epoch)
     "latitude_e7", # Latitude in deg * 1e7
@@ -16,15 +26,15 @@ UserSample = collections.namedtuple("UserSample", [
         # the next sample
 ])
 
-# Request for an infection analysis
+# Request for an infection analysis.
 Request = collections.namedtuple("Request", [
     "sick_samples", # List of UserSample-s of the infected person.
     "query_samples", # List of UserSample-s of the queried person
 ])
 
-# Result of the infection analysis
+# Result of the infection analysis.
 Response = collections.namedtuple("Response", [
-    "min_distance", # Distance of the nearest contact of the two persons
+    "min_distance", # Conservative estimate of the distance
     "score", # Infection score (higher score -> higher likelihood of infection)
 ])
 

282a077ff7ba08518fef482db35dc366fb53c258
Jan Špaček <patek.mail@gmail.com>
2020-04-01 14:51:56 +0200

    Improve the interactive map

--- util/sort_matches.py
+++ util/sort_matches.py
@@ -8,7 +8,11 @@ with open("selected_matches.json", "rt") as f_in:
         matches.append((float(m[1]), line))
 
 matches.sort(reverse=True)
-with open("matches_0.json", "wt") as f_out:
-    for _, line in matches[0:10]:
-        f_out.write(line)
-        f_out.write("\n")
+i = 0
+while len(matches) > 0:
+    with open(f"matches_{i}.json", "wt") as f_out:
+        for _, line in matches[0:10]:
+            f_out.write(line)
+            f_out.write("\n")
+    matches = matches[10:]
+    i += 1
